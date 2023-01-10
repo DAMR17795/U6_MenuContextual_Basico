e21347c79a2e9c3d394d1264a536e9685c1bd9ba
DANIEL2DAM <dmarrom577@g.educaand.es>
2023-01-10 14:38:26 +0100

    Initial commit

---
+++ app/src/main/java/www/iesmurgi/u6_menucontextual_basico/MainActivity.kt
@@ -0,0 +1,80 @@
+package www.iesmurgi.u6_menucontextual_basico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.*
+import android.widget.AdapterView
+import www.iesmurgi.u6_menucontextual_basico.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Ponemos que el menu contextual salga cuando
+        //pulsamos un rato el TextView
+        val tvResultado = binding.lblOpcion
+        registerForContextMenu(tvResultado)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View,
+                                     menuInfo: ContextMenu.ContextMenuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        return when (item.itemId) {
+            /*R.id.edit -> {
+                editNote(info.id)
+                true
+            }
+            R.id.delete -> {
+                deleteNote(info.id)
+                true
+            }*/
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+
+        return when (item.itemId) {
+            R.id.MnOp1 -> {
+                binding.lblOpcion.setText("Opcion A")
+                true
+            }
+            R.id.MnOp2 -> {
+                binding.lblOpcion.setText("Opcion B")
+                true
+            }
+            R.id.MnOp3 -> {
+                binding.lblOpcion.setText("Opcion C")
+                true
+            }
+
+            R.id.SbOp1 -> {
+                binding.lblOpcion.setText("SubOpcion 1")
+                true
+            }
+
+            R.id.SbOp2 -> {
+                binding.lblOpcion.setText("SubOpcion 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
